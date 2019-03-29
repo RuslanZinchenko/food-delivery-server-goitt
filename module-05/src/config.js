492a35b2e9d6911dfb2f8e174a1730e1431179d0
RuslanZinchenko <38359557+RuslanZinchenko@users.noreply.github.com>
2019-03-29 20:27:43 +0200

    Update config.js

--- module-05/src/config.js
+++ module-05/src/config.js
@@ -2,7 +2,7 @@ const username = "Ruslan";
 const password = "Oreipled1$";
 const options = {
   port: 3000,
-  dataBaseUrl: `mongodb+srv://${username}:${password}@cluster0-5wcez.gcp.mongodb.net/test?retryWrites=true`
+  dataBaseUrl: `mongodb+srv://${username}:${password}@cluster0-rdwdd.mongodb.net/test?retryWrites=true`
 };
 
 module.exports = options;
